5a96485acb7a7be23b0ea57c29236ae21dba640c
bapun <mrutyunjayagadanayak@yandex.com>
2025-04-29 21:35:13 +0530

    Added new table

--- sql/schema/001_schema.sql
+++ sql/schema/001_schema.sql
@@ -15,6 +15,16 @@ CREATE TABLE feeds (
     user_id UUID NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+CREATE TABLE feed_follows (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID NOT NULL,
+    feed_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (feed_id) REFERENCES feeds (id) ON DELETE CASCADE,
+    CONSTRAINT follow UNIQUE (user_id, feed_id) 
+);
 -- +goose Down
-DROP TABLE users;
 DROP TABLE feeds;
+DROP TABLE users;
